5c79c48f822b4bba517a1246bd4d003cffb57904
GH <1263887632@qq.com>
2022-04-11 16:19:57 +0800

    update APP ID

--- app/src/main/java/com/tencent/bugly/demo/MyApplication.java
+++ app/src/main/java/com/tencent/bugly/demo/MyApplication.java
@@ -5,7 +5,8 @@ import android.app.Application;
 import com.tencent.bugly.crashreport.CrashReport;
 
 /**
- * Created by yue on 2016/5/23.
+ * Created by yue on 2022/4/1.
+ * 使用流量测试，WIFI访问异常
  */
 public class MyApplication extends Application {
     @Override
@@ -17,8 +18,7 @@ public class MyApplication extends Application {
          * 参数2：APP ID，平台注册时得到,注意替换成你的appId
          * 参数3：是否开启调试模式，调试模式下会输出'CrashReport'tag的日志
          */
-        //CrashReport.initCrashReport(getApplicationContext(), "9ad897e06b", true);
         CrashReport.UserStrategy userStrategy = new CrashReport.UserStrategy(this);
-        CrashReport.initCrashReport(this, "9ad897e06b", true, userStrategy);
+        CrashReport.initCrashReport(this, "ed8d153690", true);
     }
 }
